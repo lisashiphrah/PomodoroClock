7cb88a9d5460410fc2673984e46fdbb8849e4e08
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-29 10:41:33 +0000

    added configurationButtons css and functionalities

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+
 });
+
+function modify_qty(val, id) {
+    var qty = document.getElementById(id).value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById(id).value = new_qty;
+    return new_qty;
+}
